6d98d0791cbd514f49eb35b956c6c01f1ccd31bb
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-01-28 19:21:03 +0530

    Fix shebang line and enhance script documentation for even/odd check

--- Day 1/Main.sh
+++ Day 1/Main.sh
@@ -1,4 +1,5 @@
-#!  wap in shell to see the number is even or odd.
+#!/bin/bash
+# wap in shell to see the number is even or odd.
 
 
 
@@ -8,3 +9,19 @@ if (( num % 2 == 0 )); then
 else
     echo "$num is odd."
 fi
+
+
+
+#?    How run the code
+#*    1. Open your terminal.
+#*    2. Navigate to the directory where the Main.sh file is located.
+#*    3. Make sure the script has execute permissions. You can set the permissions using the command:
+#*       chmod +x Main.sh
+#*    4. Run the script using the command:
+#*       bash "Day 1/Main.sh"
+#*    5. Follow the on-screen prompt to enter an integer and see if it is even or odd.
+#?    Example Output
+#*    Enter an integer: 7
+#*    7 is odd.
+#*    Enter an integer: 10
+#*    10 is even.
